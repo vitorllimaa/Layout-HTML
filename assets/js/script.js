3f373b79deaf6de81cd72ec63de77ad111c8cfc0
vitorlima <vitorlimavital@hotmail.com>
2021-07-13 22:04:17 -0300

    Layout de um site

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.querySelector(".open-menu").addEventListener('click', e =>{
+
+    document.querySelector("header .menu").classList.add('open');
+});
+
+document.querySelector(".close-menu").addEventListener("click", e =>{
+
+    document.querySelector("header .menu").classList.remove('open');
+});
+
+document.querySelector(".backdrop").addEventListener('click', e=> {
+    document.querySelector("header .menu").classList.remove('open');
+});
